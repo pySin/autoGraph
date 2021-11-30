bf00d5eed577e86708f69f2cc7dc2acb09e5471b
Sinan Mehmedov <47303660+pySin@users.noreply.github.com>
2021-11-30 02:22:11 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,15 @@ import math
 
 
 class GetResults:
-    pass
+    def query_1(self):
+        query = '''
+        SET @sixth = ROUND((((SELECT MAX(LifeExpectancy) FROM world.country) 
+			                 - (SELECT MIN(LifeExpectancy) FROM world.country))/6), 2); 
+        SELECT @sixth;
+        SELECT COUNT(LifeExpectancy) FROM world.country
+        WHERE LifeExpectancy BETWEEN (SELECT MIN(LifeExpectancy) + @sixth*0 FROM world.country) AND
+							         (SELECT MIN(LifeExpectancy) + @sixth*2 FROM world.country);
+        '''
 
 
 def call_functions():
